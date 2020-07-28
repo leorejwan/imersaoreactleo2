400af53af7d674f50a23b7bc7970f0ef8e9da145
leo rejwan <leorejwan@gmail.com>
2020-07-28 18:21:15 -0300

    react router dom. Ajustes no ndex.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// para que a pagina nao recarregue a cada interação
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+function CadastroVideo(){
+  return(
+    <div>
+      cuishcishcuihsuisd
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <React.StrictMode> 
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    {/* apenas de exemplo para outra rota atraves do 'react-router-dom'*/}
+    {/* tenque ir do mais especifico para o mais geral */}
+    <Route path="/cadastro/video" component={CadastroVideo} /> 
+
+    <Route path="/" component={App} exact /> 
+    {/* <Route path="/" component={App} /> */}
+
+    {/* solucao rapida para pagina 404 */}
+    <Route component={() => (<div>pagina 404</div>)}  />
+  </Switch>
+  </BrowserRouter>,
+  // <React.StrictMode> 
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
